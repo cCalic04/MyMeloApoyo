3d219c414baba30eb480c2732e0d222ba2043a0a
cCalic04 <simondiazca@gmail.com>
2025-08-21 12:45:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const myMeloReact = [
   "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymeloa6.png",
   "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymelo2t.png",
   "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymeloa8.png",
-  "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymeloa9.png"
+  "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymeloa9.png",
+  "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymeloa10.png",
+  "https://raw.githubusercontent.com/cCalic04/MyMeloApoyo/refs/heads/main/mymeloa11.png"
 ];
 
 const myMeloHelp = [
@@ -31,24 +33,26 @@ const myMeloHelp = [
     text: "Ohh, low battery :0? Do you feel calm or uncomfortable?", //nodo 2
     answers:[
       {text: "Calm", faceIndex: 7, next: 6},
-      {text: "uncomfortable", faceIndex: 5, next: 7},
+      {text: "Uncomfortable", faceIndex: 5, next: 7},
     ]
   },
   {
     text: "Just hanging in there, eh? What's going on :)?", //nodo 3
     answers:[
-      {text: "Feeling kinda down", faceIndex: 3, next: 8},
-      {text: "Feeling alright", faceIndex: 1, next: 9},
+      {text: "Feeling alright", faceIndex: 1, next: 8},
+      {text: "Feeling kinda down", faceIndex: 3, next: 9},
+
     ]
   },
   {
     text: "Nice! :D All pumped up!! Which of these sounds more like you right now?", //nodo 4
     answers:[
-      {text: "Excited -I'm feeling nice for an upcoming event", faceIndex:0, next: 10},
+      {text: "Excited -I am feeling nice for an upcoming event", faceIndex:0, next: 10},
       {text: "Euphoric -I feel great! Just really happy!", faceIndex:7, next: 10},
-      {text: "Inspired -I'm looking forward to making something of my own!", faceIndex:0, next: 10},
+      {text: "Inspired -I amlooking forward to making something of my own!", faceIndex:0, next: 10},
       {text: "Pleasantly surprised -Something unexpected happened and it makes me really happy!", faceIndex: 0, next: 10},
       {text: "Cheerful -I just did something really funny, I can't stop laughing and smiling!", faceIndex: 7, next: 10},
+      {text: "Focused -I am really invested in something I'm doing!", faceIndex: 0, next: 10},
     ]
   },
   {
@@ -59,8 +63,76 @@ const myMeloHelp = [
       {text: "Frustrated -I can't do anything right!", faceIndex:8, next: 10},
       {text: "Defensive -I don't deserve the way people are treating me, it's not fair!", faceIndex: 5, next: 10},
       {text: "Panicked -Something really threw me off and I am having trouble staying on track",faceIndex: 6, next: 10},
+      {text: "In a hurry -So much stuff to do!! I need to do it all as soon as possible", faceIndex: 3, next: 10},
     ]
-  }
+  },
+  {
+    text: "Good to hear you are feeling cozy :) Which of these sounds more like you right now?", //nodo 6
+    answers: [
+      {text: "Fulfilled -Something I did is paying off, feels good", faceIndex: 10, next: 10},
+      {text: "Relieved -In the end, everything was alright...", faceIndex: 11, next: 10},
+      {text: "Carefree -Nothing to do at the moment, I can relax for a bit", faceIndex: 0, next: 10},
+      {text: "Sleepy -Finally time to rest after a long day", faceIndex: 10, next: 10},
+      {text: "Content -I am just feeling alright at the moment", faceIndex: 10, next: 10},
+      {text: "Grateful -Something came out just the way I needed it to!", faceIndex: 10, next: 10},
+    ]
+  },
+  {
+    text: "Oh, long day :(? Which of these sounds more like you right now?", //nodo 7
+    answers: [
+      {text: "Melancholic -I am feeling contemplative and introspective", faceIndex: 4, next: 10},
+      {text: "Apathetic -Nothing seems to interests me right now, I don't want to engage in anything", faceIndex: 11, next: 10},
+      {text: "Overwhelmed -There's too much to do and I don't have energy left for any of it", faceIndex: 11, next: 10},
+      {text: "Despair -I feel hopeless about my future, I'm lost", faceIndex: 11, next: 10},
+      {text: "Drained -I am about to go out of battery...", faceIndex: 6, next: 10},
+      {text: "Miserable -I am feeling just desperately sad and I can't seem to be able to overcome it", faceIndex: 6, next: 10},
+    ]  
+  },
+    {
+    text: "Glad to hear that :) Which of these sounds more like you right now?", //nodo 8
+    answers: [
+      {text: "Pleasant -Things are coming along nicely today", faceIndex: 0, next: 10},
+      {text: "Easygoing -I am worriless, just vibing", faceIndex: 10, next: 10},
+      {text: "Secure -Things have been going my way lately", faceIndex: 0, next: 10},
+      {text: "Comfy -This place is very nice...", faceIndex: 10, next: 10},
+      {text: "Hopeful -I am positive about my future", faceIndex: 7, next: 10},
+      {text: "Restful -I have just the right amount of energy", faceIndex: 8, next: 10},
+    ]
+  },
+  {
+    text: "Oh okay :( Which of these sounds more like you right now?", //nodo 9
+    answers: [
+      {text: "Bored -Nothing to do right now...", faceIndex: 4, next: 10},
+      {text: "Dissapointed -Something didn't came out the way I expected it to", faceIndex: 5, next: 10},
+      {text: "Irritated -Most things seem to be annoying me right now", faceIndex: 8, next: 10},
+      {text: "Annoyed -Something in particular threw me off, not enough to make me mad, but still", faceIndex: 8, next: 10},
+      {text: "Nervous -I am feeling a bit agitated", faceIndex: 4, next: 10},
+      {text: "Startled -Something sudden surprised me! I'm still processing it", faceIndex: 3, next: 10},
+    ]
+  },
+  {
+    text: "Now we know how you are feeling :D, but what made you feel like this?", //nodo 10
+    answers: [
+      {text: "A person or their actions -Somebody did something particularly to me", faceIndex: 0, next: 11},
+      {text: "The place I'm at -Being here really changes how I feel", faceIndex: 0, next: 11},
+      {text: "Something I saw -This object seemed to have an impact in me, either that or it made me remember something emotional", faceIndex: 0, next: 11},
+      {text: "The day I've been having -My current emotions are the sum of this whole day", faceIndex: 0, next: 11},
+      {text: "Something I was told -A story (a tv show, a book, a song, gossip, etc) made me feel a particular way", faceIndex: 0, next: 11},
+      {text: "A memory -I remembered something that happened to me", faceIndex: 0, next: 11},
+    ]
+  },
+  {
+    text: "Okay, now, how would you feel about being with other people right now?", //nodo 11
+    answers:[
+        {text: "I strongly want to be with someone in particular right now", faceIndex: 0, next: 12},
+        {text: "I want to be with someone in particular right now", faceIndex: 0, next: 12},
+        {text: "Right now, I don't feel any particular way about company", faceIndex: 0, next: 12},
+        {text: "I don't want to be alone, but I don't really care who I am with at the moment", faceIndex: 0, next: 12},
+        {text: "I want to have some time for myself right now", faceIndex: 0, next: 12},
+        {text: "I strongly want to have some time for myself right now", faceIndex: 0, next: 12},
+        {text: "I want to avoid someone in particular right now", faceIndex: 0, next: 12},
+    ]
+  },
   ]
 const dialogues = [
   {
